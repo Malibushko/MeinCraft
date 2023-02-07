655b7c957e161b1438c19023f63b01c9e3b65a02
malibushko <Kostya28837a1>
2023-02-07 19:30:38 +0200

    init

---
+++ core/entity/Entity.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <entt/entity/entity.h>
+
+using entity_t = entt::entity::entity_t;
---
+++ game/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "core/entity/entity.h"
+
+int main()
+{
+  std::cout << "Hello, world!" << std::endl;
+}
